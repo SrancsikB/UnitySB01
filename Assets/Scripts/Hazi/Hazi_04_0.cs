d440321e22f6776035a7da37341bda227d9c0dc7
SrancsikB <balint.srancsik@gmail.com>
2025-03-01 08:56:25 +0100

    Rot

---
+++ Assets/Scripts/Hazi/Hazi_04_0.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class Hazi_04_0 : MonoBehaviour
+{
+    [SerializeField] float rotSpeed = 60;
+    [SerializeField] float angleToRotate = 90;
+    float targetAngle;
+    int rotDirection;
+
+    void Update()
+    {
+        float currentAngle = transform.rotation.eulerAngles.z;
+        if (rotDirection == 0)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                targetAngle = currentAngle + angleToRotate;
+                rotDirection = 1;
+            }
+            else if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                targetAngle = currentAngle - angleToRotate;
+                rotDirection = -1;
+            }
+        }
+        else
+        {
+            transform.RotateAround(Vector3.zero, Vector3.forward * rotDirection, Time.deltaTime * rotSpeed);
+            if (rotDirection > 0 && currentAngle >= targetAngle || rotDirection < 0 && currentAngle <= targetAngle)
+            {
+                rotDirection = 0;
+                transform.rotation = Quaternion.Euler(0, 0, targetAngle);
+            }
+
+        }
+
+
+    }
+}
